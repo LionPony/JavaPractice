16235bfec43d648e92ad1b44e873edf2e91335fe
LionPony <146934556+LionPony@users.noreply.github.com>
2023-10-05 17:24:33 +0900

    add Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,6 @@
+//num1, num2 are integer. return num1 - num2. integer range -50000 <=, <=50000
+public class Subtraction {
+    public static int sub(int num1, int num2){
+        return num1-num2;
+    }
+}
--- TestSet.java
+++ TestSet.java
@@ -2,16 +2,16 @@
 import java.util.Scanner;
 public class TestSet {
     public static void main(String[] args){
-        test1();
-    }
-    //Calculate share between two nums(integer).
-    public static void test1(){
-        int num1;
-        int num2;
-        Scanner sc = new Scanner(System.in);
 
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
+    }
+
+    //Calculate share between two numbers(integer).
+    public static void test1(int num1, int num2){
         System.out.println(Divide.share(num1, num2));
+    }
+
+    //Subtraction test(num1 - num2)
+    public static void test2(int num1, int num2){
+        System.out.println(Subtraction.sub(num1, num2));
     }
 }
